044aa2ea9847a61faf9ca15a7acb9f61861ca707
Dariia Kachalova <87769278+dariia-kachalova@users.noreply.github.com>
2022-08-27 12:10:19 +0200

    Add project files.

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values ('James', 'Smith'),
+	 ('Ozzy', 'Osbourne'),
+	 ('Janis', 'Joplin'),
+	 ('John', 'Lennon');
+end
